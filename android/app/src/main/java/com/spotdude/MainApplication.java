b359752ba9ac5302684e6c51fe3a794efe7f41d3
cev <seeve.c@gmail.com>
2018-02-21 18:14:53 -0500

    added react-native  maps

--- android/app/src/main/java/com/spotdude/MainApplication.java
+++ android/app/src/main/java/com/spotdude/MainApplication.java
@@ -3,10 +3,13 @@ package com.spotdude;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new MapsPackage()
       );
     }
 
@@ -31,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       return "index";
     }
   };
+  
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -42,4 +47,5 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
+  
 }
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, ToastAndroid, StyleSheet } from 'react-native';
-
+import MapView from 'react-native-maps';
 import data from '../Global/fakeData';
 import CardList from './Components/CardList';
 class HomePage extends Component {
@@ -10,6 +10,17 @@ class HomePage extends Component {
         return (
             <View style={styles.CardList}>
                 <CardList messages={data} />
+                <View style={styles.container}>
+                    <MapView
+                        style={styles.map}
+                        region={{
+                            latitude: 45.5017,
+                            longitude: -73.5673,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421
+                        }}
+                    />
+                </View>
             </View>
         );
     }
@@ -19,6 +30,22 @@ const styles = StyleSheet.create({
     CardList: {
         flex: 1,
         margin: 40
+    },
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
     }
 });
 export default HomePage;
