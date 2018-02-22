3a13474d99aa9bcb92fb6366e44b9fe958093a64
cev <seeve.c@gmail.com>
2018-02-22 11:28:46 -0500

    stuff

--- src/Global/App.js
+++ src/Global/App.js
@@ -1,16 +1,18 @@
 import { Navigation } from 'react-native-navigation';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
-import AuthScreen from '../screens/Auth';
+import Login from '../screens/Authentication/login';
+import Signup from '../screens/Authentication/signup';
 
 //React native requires a wrapper to render components
 //for each no screen, you must register the component
-Navigation.registerComponent('spot.SplashScreen', () => AuthScreen);
+Navigation.registerComponent('spot.LoginScreen', () => Login);
+Navigation.registerComponent('spot.SignupScreen', () => Signup);
 Navigation.registerComponent('spot.HomeScreen', () => HomeScreen);
 
 //Start the react Navigation wrapped App
 Navigation.startSingleScreenApp({
     screen: {
-        screen: 'spot.SplashScreen',
+        screen: 'spot.SignupScreen',
         title: 'Welcome'
     }
 });
