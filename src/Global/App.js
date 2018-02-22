f819736f0697d9af979ecbbfda063f9984d00871
jytsong <jyt.song@gmail.com>
2018-02-22 17:19:54 -0500

    registered NewLocation component in App.js

--- src/Global/App.js
+++ src/Global/App.js
@@ -1,25 +1,27 @@
-import { Navigation } from 'react-native-navigation';
-import HomeScreen from '../screens/HomeScreen/HomeScreen';
-import Login from '../screens/Authentication/login';
-import Signup from '../screens/Authentication/signup';
-import AddMessagesScreen from '../screens/AddMessagesScreen/AddMessagesScreen';
-import LocationScreen from '../screens/LocationScreen/LocationScreen';
-import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
-import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
+import { Navigation } from "react-native-navigation";
+import HomeScreen from "../screens/HomeScreen/HomeScreen";
+import Login from "../screens/Authentication/login";
+import Signup from "../screens/Authentication/signup";
+import AddMessagesScreen from "../screens/AddMessagesScreen/AddMessagesScreen";
+import LocationScreen from "../screens/LocationScreen/LocationScreen";
+import ProfileScreen from "../screens/ProfileScreen/ProfileScreen";
+import SettingsScreen from "../screens/SettingsScreen/SettingsScreen";
+import NewLocation from "../screens/AddMessagesScreen/newLocation";
 //React native requires a wrapper to render components
 //for each no screen, you must register the component
-Navigation.registerComponent('spot.LoginScreen', () => Login);
-Navigation.registerComponent('spot.SignupScreen', () => Signup);
-Navigation.registerComponent('spot.AddMessagesScreen', () => AddMessagesScreen);
-Navigation.registerComponent('spot.LocationScreen', () => LocationScreen);
-Navigation.registerComponent('spot.ProfileScreen', () => ProfileScreen);
-Navigation.registerComponent('spot.SettingsScreen', () => SettingsScreen);
-Navigation.registerComponent('spot.HomeScreen', () => HomeScreen);
+Navigation.registerComponent("spot.LoginScreen", () => Login);
+Navigation.registerComponent("spot.SignupScreen", () => Signup);
+Navigation.registerComponent("spot.AddMessagesScreen", () => AddMessagesScreen);
+Navigation.registerComponent("spot.NewLocationScreen", () => NewLocation);
+Navigation.registerComponent("spot.LocationScreen", () => LocationScreen);
+Navigation.registerComponent("spot.ProfileScreen", () => ProfileScreen);
+Navigation.registerComponent("spot.SettingsScreen", () => SettingsScreen);
+Navigation.registerComponent("spot.HomeScreen", () => HomeScreen);
 
 //Start the react Navigation wrapped App
 Navigation.startSingleScreenApp({
-    screen: {
-        screen: 'spot.LoginScreen',
-        title: 'Welcome'
-    }
+  screen: {
+    screen: "spot.LoginScreen",
+    title: "Welcome"
+  }
 });
