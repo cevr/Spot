24ef2833cf3d4e24b1f6d614b002d70272cf756f
cev <seeve.c@gmail.com>
2018-02-22 01:16:29 -0500

    changed file structure to reflect new app structure

--- src/Global/App.js
+++ src/Global/App.js
@@ -1,9 +1,12 @@
 import { Navigation } from 'react-native-navigation';
-import HomeScreen from '../HomeScreen/HomeScreen';
-import AuthScreen from '../screens/Auth/Auth';
-//React native requires a wrapper to render component
+import HomeScreen from '../screens/HomeScreen/HomeScreens';
+import AuthScreen from '../screens/Auth';
+
+//React native requires a wrapper to render components
+//for each no screen, you must register the component
 Navigation.registerComponent('spot.SplashScreen', () => AuthScreen);
 Navigation.registerComponent('spot.HomeScreen', () => HomeScreen);
+
 //Start the react Navigation wrapped App
 Navigation.startSingleScreenApp({
     screen: {
--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,10 +1,12 @@
 //Vako please put your code in here
+//placeholder code
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import startTabs from '../Tabs/startMainTab';
+import startTabs from '../Tabs/startTabs';
 
 class AuthScreen extends Component {
     onPressHandler = () => {
+        //this function changes the navigation to render a tabbed based app
         startTabs();
     };
 
