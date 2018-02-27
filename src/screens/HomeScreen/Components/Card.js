9e94323a92d53ae7138ad8b3c81c58da59db70e8
cev <seeve.c@gmail.com>
2018-02-27 17:39:28 -0500

    styles

--- src/screens/HomeScreen/Components/Card.js
+++ src/screens/HomeScreen/Components/Card.js
@@ -13,7 +13,6 @@ import { H3, H2 } from '../../../UI';
 export default (Card = props => (
     // This prop is usually called at the CardList component
     <Touchable
-        elevation={5}
         onPress={() => {
             props.navigator.showModal({
                 screen: 'spot.CardPopUp',
@@ -25,8 +24,17 @@ export default (Card = props => (
             });
         }}
     >
-        <View style={styles.Card}>
-            <H3 style={styles.text}>{props.cardData.title}</H3>
+        <View
+            elevation={5}
+            style={props.cardData.read ? styles.CardRead : styles.CardUnread}
+        >
+            <H3
+                style={
+                    props.cardData.read ? styles.textRead : styles.textUnread
+                }
+            >
+                {props.cardData.title}
+            </H3>
             <View
                 style={{
                     flexDirection: 'row',
@@ -38,21 +46,37 @@ export default (Card = props => (
 ));
 
 const styles = StyleSheet.create({
-    Card: {
+    CardRead: {
         width: '100%',
         height: 70,
-        backgroundColor: '#c43639',
         marginBottom: 8,
         padding: 7,
         flexDirection: 'column',
         alignItems: 'flex-start',
         justifyContent: 'center',
         alignItems: 'center',
-        borderColor: '#aaa',
-        borderWidth: 4,
-        borderRadius: 7
+        borderWidth: 2,
+        borderRadius: 7,
+        backgroundColor: '#F6F6F6',
+        borderColor: '#aaa'
     },
-    text: {
-        color: '#F6F6F6'
+    CardUnread: {
+        width: '100%',
+        height: 70,
+        marginBottom: 8,
+        padding: 7,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 7,
+        borderWidth: 3,
+        borderColor: '#ce797b',
+
+        backgroundColor: '#F6F6F6'
+    },
+    textRead: {},
+    textUnread: {
+        color: '#ce797b'
     }
 });
